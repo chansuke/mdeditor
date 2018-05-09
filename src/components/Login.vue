069d244633f4aa70f66832aea0b5a1608060c54b
chansuke <chansuke@georepublic.de>
2018-05-09 23:49:22 +0900

    Add components

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id='login'>
+    <h1>{{ msg }}</h1>
+    <button>ログイン</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      msg: 'This is a markdown editor'
+    };
+  }
+};
+</script>
